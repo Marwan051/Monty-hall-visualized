96dca08e09a56117ce51e0f463b3c37e9558eecb
Marwan051 <115355087+Marwan051@users.noreply.github.com>
2023-09-02 15:36:40 +0300

    Update MontyHall.py

--- MontyHall.py
+++ MontyHall.py
@@ -2,6 +2,15 @@ import random
 
 
 def startgame(num, dnum, mode, losstowinratio):
+    if num < 1:
+        print("Invalid Input")
+        exit()
+    elif dnum < 3:
+        print("Invalid Input")
+        exit()
+    elif mode < 1 or mode > 3:
+        print("Invalid Input")
+        exit()
     # make the hashmap for the simulated doors
     game = dict()
     choice = 0
@@ -24,7 +33,7 @@ def startgame(num, dnum, mode, losstowinratio):
             else:
                 losstowinratio[1] += 1
 
-        elif 3:  # control case(changing or no changing randomly)
+        elif mode == 3:  # control case(changing or no changing randomly)
             ranchoice = (int)(random.random() * 2 + 1)
             if ranchoice == 1:
                 if game.get(choice) == 1:
@@ -41,14 +50,20 @@ def startgame(num, dnum, mode, losstowinratio):
         game.clear()
 
 
-losstowinratio = [
-    0,
-    0,
-]  # array for the results (first value is losses the second is wins)
-sampleSize = 1  # min is 1
-numOfDoors = 3  # min is 3
-mode = 1  # modes are (1:no change,2:always change,3:Control-random change-)
-startgame(sampleSize, numOfDoors, mode, losstowinratio)  # running the simulation
-print(
-    "Losses: " + str(losstowinratio[0]) + "\nWins: " + str(losstowinratio[1])
-)  # printing the results
+def main():
+    losstowinratio = [
+        0,
+        0,
+    ]  # array for the results (first value is losses the second is wins)
+    sampleSize = int(input("Enter Sample size (min 1):"))  # min is 1
+    numOfDoors = int(input("Enter number of doors (min 3):"))  # min is 3
+    mode = int(
+        input("Enter mode (1,2,3):")
+    )  # modes are (1:no change,2:always change,3:Control-random change-)
+    startgame(sampleSize, numOfDoors, mode, losstowinratio)  # running the simulation
+    print(
+        "Losses: " + str(losstowinratio[0]) + "\nWins: " + str(losstowinratio[1])
+    )  # printing the results
+
+
+main()
